aed6d049a8873eeff9fbc6979709679c544d571d
Milan Fox <milan.fox@diva-e.com>
2025-01-13 22:58:09 +0100

    add InputForm component

---
+++ components/molecules/InputForm/InputForm.types.ts
@@ -0,0 +1,13 @@
+import type { TextInputProps } from '@atoms/TextInput/TextInput.types';
+import type { Reactive } from 'vue';
+
+export interface FormField {
+  model: any;
+  data: TextInputProps;
+}
+
+export interface InputFormProps {
+  formFields: Reactive<FormField[]>;
+  onSubmit: () => any;
+  submitLabel?: string;
+}
