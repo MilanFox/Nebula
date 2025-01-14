17456ede940e608595c7eac185b4603e6d684a7e
Milan Fox <milan.fox@linuxmail.org>
2025-01-14 23:54:32 +0100

    add AccordionItem component

---
+++ components/atoms/AccordionItem/AccordionItem.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class=accordion-item :class="{'accordion-item--expanded': isExpanded}">
+    <h3 class="accordion-item__headline">
+      <button
+        :aria-expanded="isExpanded"
+        type="button"
+        :aria-controls="id"
+        @click="toggleAccordion"
+        class="accordion-item__button"
+      >
+        {{ title }}
+      </button>
+    </h3>
+    <div :id="id" class="accordion-item__content" :aria-hidden="!isExpanded">
+      <slot/>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { AccordionItemProps } from './AccordionItem.types';
+
+const props = defineProps<AccordionItemProps>();
+
+const toKebabCase = (str: string) => str.replace(/([a-z])([A-Z])/g, '$1-$2').replace(/[\s_]+/g, '-').toLowerCase();
+const id = `accordion-${toKebabCase(props.title)}`;
+
+const isExpanded = ref(false);
+const toggleAccordion = () => isExpanded.value = !isExpanded.value;
+</script>
+
+<style lang="scss">
+@use 'assets/scss/colors';
+
+.accordion-item {
+  display: grid;
+  grid-template-rows: min-content 0fr;
+  transition: grid-template-rows ease-in-out 0.3s;
+
+  &__headline {
+    margin: 0;
+    display: flex;
+  }
+
+  &__button {
+    all: unset;
+    cursor: pointer;
+    position: relative;
+    padding: 4px 40px 4px 0;
+    flex-grow: 1;
+    align-items: flex-start;
+
+    &::after {
+      content: '';
+      position: absolute;
+      right: 16px;
+      transform: rotateZ(405deg);
+      transition: transform ease-in-out 0.3s;
+      display: inline-block;
+      width: 16px;
+      height: 16px;
+      border-bottom: 4px solid colors.$blue;
+      border-right: 4px solid colors.$blue;
+    }
+
+    &:hover {
+      color: colors.$blue;
+    }
+  }
+
+  &__content {
+    overflow: hidden;
+  }
+
+  &--expanded {
+    grid-template-rows: min-content 1fr;
+
+    .accordion-item__button::after {
+      transform: rotateZ(225deg);
+    }
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -26,7 +26,36 @@
     </GridLayout>
   </ContentSection>
   <ContentSection title="Frequently Asked Questions">
-    <AccordionGroup/>
+    <AccordionItem title="This looks an awful lot like Advent of Code">
+      <p>First of all it doesn't <em>look</em> like AoC. I've put way more emphasis on the frontend. 😉</p>
+      <p>But yes, this is very heavily inspired by it. I was looking for a fun project and thought of this to spread the
+        puzzle love a bit. The puzzles in this project are a lot easier and less algorithm-heavy though to make it more
+        accessible to beginners.</p>
+    </AccordionItem>
+    <AccordionItem title="How does this work?">
+      <p>Every puzzles task is clearly described inside the puzzle - you might interpret it a bit though, otherwise it
+        won't be a puzzle, right?</p>
+      <p>Each puzzle results in a clear answer, which for now is <em>always</em> going to be a number of some sort.</p>
+    </AccordionItem>
+    <AccordionItem title="My solution isn't being accepted, why?">
+      <p>There's three possible answers here:</p>
+      <p><em>Your Solution</em> is just wrong. I've put a lot of effort into ensuring that the validation logic works.
+        There are automatic tests in place and I had them manually tested.</p>
+      <p><em>My</em> input-generation or validation logic is wrong. That can happen. If you suspect an error on my end,
+        just report it to me!</p>
+      <p>You have the solution for a different input. Every Account gets a different input to make it more fun.
+        Especially if you've logged in anonymously, your output might have changed in between logins or devices. If you
+        want to ensure that your input stays consistent, then consider signing in for an actual account.</p>
+    </AccordionItem>
+    <AccordionItem title="Can't I just use AI?">
+      <p>Yup - nothing is stopping you... Actually the puzzles are simple enough that I have no doubt even an older
+        version of ChatGPT will be able to solve them. But what would be the point there? You would only cheat
+        yourself.</p>
+    </AccordionItem>
+    <AccordionItem title="Can I suggest a puzzle?">
+      <p>Sure. Why not!</p>
+      <p>If you look at the Backend Repo you will find all puzzles. Just follow the format and make a pull request.</p>
+    </AccordionItem>
   </ContentSection>
 </template>
 
@@ -76,3 +105,5 @@
   }
 }
 </style>
+<script setup lang="ts">
+</script>
