4666a4db1ddf835d37b38c35c4ad9fe54f4b8766
Milan Fox <milan.fox@linuxmail.org>
2025-01-14 12:53:49 +0100

    add HeaderBar component

---
+++ components/molecules/HeaderBar/HeaderBar.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="header-bar">
+    <a href="/"><img src="assets/images/logo.svg" alt="Home" class="header-bar__logo"></a>
+    <nav class="header-bar__nav">
+      <MainNav/>
+      <AuthButton/>
+    </nav>
+  </div>
+</template>
+
+<style lang="scss">
+.header-bar {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 64px;
+
+  &__logo {
+    width: 200px;
+  }
+
+  &__nav {
+    display: flex;
+    gap: 128px;
+    align-items: center;
+  }
+}
+</style>
+<script setup lang="ts">
+import MainNav from '@molecules/MainNav/MainNav.vue';
+</script>
