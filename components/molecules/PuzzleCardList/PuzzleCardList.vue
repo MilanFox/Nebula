fbac6e7a07ad0a89db50be20b9c37088c2ad609a
Milan Fox <milan.fox@linuxmail.org>
2025-01-15 15:49:10 +0100

    add PuzzleCardList component

---
+++ components/molecules/PuzzleCardList/PuzzleCardList.vue
@@ -0,0 +1,19 @@
+<template>
+  <ul class=puzzle-card-list>
+    <PuzzleCard v-for="puzzleData in publishedPuzzles" :key="puzzleData.id" v-bind="puzzleData"/>
+  </ul>
+</template>
+
+<script setup lang="ts">
+import { publishedPuzzles } from '~/lib/puzzles/puzzles.const';
+</script>
+
+<style lang="scss">
+.puzzle-card-list {
+  list-style: none;
+  display: flex;
+  gap: 32px;
+  padding: 0;
+  flex-wrap: wrap;
+}
+</style>
