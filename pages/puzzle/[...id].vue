c4474e2e88949ff7c88d23c12ac24525e4ba5954
Milan Fox <milan.fox@linuxmail.org>
2025-01-15 15:49:43 +0100

    add puzzle links to home page

---
+++ pages/puzzle/[...id].vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="puzzle-page">
+    {{ puzzle }}
+  </div>
+</template>
+
+<script setup lang="ts">
+import { publishedPuzzles } from '~/lib/puzzles/puzzles.const';
+
+const route = useRoute();
+const [id] = route.params.id;
+
+if (!publishedPuzzles.some(puzzle => puzzle.id === id)) throw createError({
+  statusCode: 404,
+  statusMessage: 'This puzzle doesn\'t exist yet or isn\'t published.',
+});
+
+const puzzle = publishedPuzzles.find(puzzle => puzzle.id === id);
+</script>
