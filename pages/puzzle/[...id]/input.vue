f7de31642bf0212ee50999118e3ecd12966db18e
Milan Fox <milan.fox@linuxmail.org>
2025-01-16 22:22:00 +0100

    Create Server Route for Puzzle Data

---
+++ pages/puzzle/[...id]/input.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="input-page">
+    {{ input }}
+  </div>
+</template>
+
+<script setup lang="ts">
+import { puzzles } from '@puzzles';
+import { useUserStore } from '~/stores/userStore';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+
+const route = useRoute();
+const [id] = route.params.id;
+
+useHead({ title: `Nebula - Puzzle ${id} - Input` });
+
+const i = Number(id) - 1;
+
+if (!puzzles[i]) throw createError({
+  statusCode: 404,
+  statusMessage: 'This puzzle doesn\'t exist yet or isn\'t published.',
+});
+
+const input = ref();
+const output = ref();
+
+onAuthStateChanged(getAuth(), async (user) => {
+  if (!user) {
+    input.value = undefined;
+    output.value = undefined;
+    return;
+  }
+  const { hashUUID } = useUserStore();
+  const seed = hashUUID(user.uid);
+  const { data } = await useFetch('/api/puzzle', { method: 'POST', body: { seed, id } });
+  input.value = data.value?.input;
+  output.value = data.value?.output;
+});
+</script>
+
+<style lang="scss">
+.input-page {
+  word-break: break-word;
+}
+</style>
